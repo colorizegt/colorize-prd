b97019412b802d3f681d66d33e88c0b80cde75e9
Colorize <97766820+colorizegt@users.noreply.github.com>
2026-08-14 12:49:09 -0600

    Update pos_order.js

--- pos_discount_manager/static/src/js/pos_order.js
+++ pos_discount_manager/static/src/js/pos_order.js
@@ -4,11 +4,20 @@ import { Order } from "@point_of_sale/app/store/models";
 import { patch } from "@web/core/utils/patch";
 
 patch(Order.prototype, {
-    setup() {
-        super.setup(...arguments);
+    export_as_JSON() {
+        const json = super.export_as_JSON(...arguments);
 
-        this.discount_manager_id = false;
-        this.discount_manager_name = false;
+        json.discount_manager_id = this.discount_manager_id || false;
+        json.discount_manager_name = this.discount_manager_name || false;
+
+        return json;
+    },
+
+    init_from_JSON(json) {
+        super.init_from_JSON(...arguments);
+
+        this.discount_manager_id = json.discount_manager_id || false;
+        this.discount_manager_name = json.discount_manager_name || false;
     },
 
     set_discount_manager(employee) {
@@ -23,24 +32,8 @@ patch(Order.prototype, {
 
     get_discount_manager() {
         return {
-            id: this.discount_manager_id,
-            name: this.discount_manager_name,
+            id: this.discount_manager_id || false,
+            name: this.discount_manager_name || false,
         };
-    },
-
-    export_as_JSON() {
-        const json = super.export_as_JSON(...arguments);
-
-        json.discount_manager_id = this.discount_manager_id;
-        json.discount_manager_name = this.discount_manager_name;
-
-        return json;
-    },
-
-    init_from_JSON(json) {
-        super.init_from_JSON(...arguments);
-
-        this.discount_manager_id = json.discount_manager_id || false;
-        this.discount_manager_name = json.discount_manager_name || false;
     },
 });
