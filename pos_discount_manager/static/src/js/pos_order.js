902ae5d8850aeb1f30c372ff18d7ccd889a38509
Colorize <97766820+colorizegt@users.noreply.github.com>
2026-08-14 13:09:09 -0600

    Update pos_order.js

--- pos_discount_manager/static/src/js/pos_order.js
+++ pos_discount_manager/static/src/js/pos_order.js
@@ -4,36 +4,31 @@ import { Order } from "@point_of_sale/app/store/models";
 import { patch } from "@web/core/utils/patch";
 
 patch(Order.prototype, {
+
+    setup() {
+        super.setup(...arguments);
+
+        this.discount_authorized = false;
+        this.discount_manager_id = false;
+        this.discount_manager_name = false;
+    },
+
+    init_from_JSON(json) {
+        super.init_from_JSON(...arguments);
+
+        this.discount_authorized = json.discount_authorized || false;
+        this.discount_manager_id = json.discount_manager_id || false;
+        this.discount_manager_name = json.discount_manager_name || false;
+    },
+
     export_as_JSON() {
         const json = super.export_as_JSON(...arguments);
 
+        json.discount_authorized = this.discount_authorized || false;
         json.discount_manager_id = this.discount_manager_id || false;
         json.discount_manager_name = this.discount_manager_name || false;
 
         return json;
     },
 
-    init_from_JSON(json) {
-        super.init_from_JSON(...arguments);
-
-        this.discount_manager_id = json.discount_manager_id || false;
-        this.discount_manager_name = json.discount_manager_name || false;
-    },
-
-    set_discount_manager(employee) {
-        if (employee) {
-            this.discount_manager_id = employee.id;
-            this.discount_manager_name = employee.name;
-        } else {
-            this.discount_manager_id = false;
-            this.discount_manager_name = false;
-        }
-    },
-
-    get_discount_manager() {
-        return {
-            id: this.discount_manager_id || false,
-            name: this.discount_manager_name || false,
-        };
-    },
 });
