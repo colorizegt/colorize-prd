c9999d8ef346c351df9ed5720902cd0e20667abc
Colorize <97766820+colorizegt@users.noreply.github.com>
2026-09-09 17:33:57 -0600

    Update res_partner.py

--- fel_gt/models/res_partner.py
+++ fel_gt/models/res_partner.py
@@ -1,7 +1,8 @@
-# -*- encoding: utf-8 -*-
-
 from odoo import models, fields, api, _
 import logging
+
+_logger = logging.getLogger(__name__)
+
 
 class Partner(models.Model):
     _inherit = "res.partner"
