1d4a09e1243453741ab9e65a11359f73883b6644
Colorize <97766820+colorizegt@users.noreply.github.com>
2026-09-10 12:13:57 -0600

    Update hide_chatter.js

--- simplify_access_management/static/src/js/hide_chatter.js
+++ simplify_access_management/static/src/js/hide_chatter.js
@@ -1,65 +1,70 @@
-/* @odoo-module */
-
+/** @odoo-module **/
 import { FormRenderer } from "@web/views/form/form_renderer";
-import { ListController } from "@web/views/list/list_controller";
-import { FormController } from "@web/views/form/form_controller";
-import { session } from "@web/session";
 import { patch } from "@web/core/utils/patch";
-import { jsonrpc } from "@web/core/network/rpc_service";
 import { useService } from "@web/core/utils/hooks";
-
 import { onMounted } from "@odoo/owl";
+import { session } from "@web/session";
 
 patch(FormRenderer.prototype, {
   setup() {
     super.setup();
     this.orm = useService("orm");
-    const self = this;
 
-    return Promise.resolve(super.setup()).then(function (ev) {
-      var hash = window.location.hash.replace("#", '').split("&");
+    onMounted(async () => {
+      // Odoo 19: obtener cids del hash o session
+      const hash = window.location.hash.replace("#", "").split("&");
       let cids;
-      if(hash.findIndex(ele => ele.includes("cid")) == -1)
-          cids = session.company_id;
-      else {
-          cids = hash.filter(ele => ele.includes("cid"))[0].split("=")[1].split(",");
-          cids = cids.length > 0? parseInt(cids[0]): session.company_id;
+      const cidIndex = hash.findIndex((ele) => ele.includes("cid"));
+      if (cidIndex === -1) {
+        cids = session.company_id;
+      } else {
+        const cidList = hash[cidIndex].split("=")[1].split(",");
+        cids = cidList.length > 0 ? parseInt(cidList[0]) : session.company_id;
       }
-      let model = hash.filter(ele=>ele.includes("model"))?.[0];
-      model = model? model.split("=")?.[1].split(",")?.[0]: model;
-      if (cids && model) {
-        self.orm
-          .call("access.management", "get_chatter_hide_details", [
-            session.user_id,
-            cids,
-            model,
-          ])
-          .then(function (result) {
-            if (!result["hide_send_mail"]) {
-              var btn1 = setInterval(function () {
-                if ($(".o-mail-Chatter-sendMessage").length) {
-                  $(".o-mail-Chatter-sendMessage").remove();
-                  clearInterval(btn1);
-                }
-              }, 50);
-            }
-            if (!result["hide_log_notes"]) {
-              var btn2 = setInterval(function () {
-                if ($(".o-mail-Chatter-logNote").length) {
-                  $(".o-mail-Chatter-logNote").remove();
-                  clearInterval(btn2);
-                }
-              }, 50);
-            }
-            if (!result["hide_schedule_activity"]) {
-              var btn3 = setInterval(function () {
-                if ($(".o-mail-Chatter-activity").length) {
-                  $(".o-mail-Chatter-activity").remove();
-                  clearInterval(btn3);
-                }
-              }, 50);
-            }
-          });
+
+      let model;
+      const modelEntry = hash.find((ele) => ele.includes("model"));
+      if (modelEntry) {
+        model = modelEntry.split("=")?.[1].split(",")?.[0];
+      }
+      if (!model) {
+        model = this.props?.resModel;
+      }
+
+      if (!cids || !model) {
+        return;
+      }
+
+      try {
+        const result = await this.orm.call(
+          "access.management",
+          "get_chatter_hide_details",
+          [session.user_id, cids, model]
+        );
+
+        // Odoo 19: usar CSS classes en lugar de jQuery
+        const hideClasses = [];
+        if (!result["hide_send_mail"]) {
+          hideClasses.push(".o-mail-Chatter-sendMessage");
+        }
+        if (!result["hide_log_notes"]) {
+          hideClasses.push(".o-mail-Chatter-logNote");
+        }
+        if (!result["hide_schedule_activity"]) {
+          hideClasses.push(".o-mail-Chatter-activity");
+        }
+
+        if (hideClasses.length) {
+          const style = document.createElement("style");
+          style.id = "sam-hide-chatter-style";
+          style.textContent = `${hideClasses.join(", ")} { display: none !important; }`;
+          document.head.appendChild(style);
+        }
+      } catch (error) {
+        console.warn(
+          "[simplify_access_management] Error al ocultar chatter:",
+          error
+        );
       }
     });
   },
