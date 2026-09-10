8d7778137c0c272b013c9b14b94eabf1cd5f0faa
Colorize <97766820+colorizegt@users.noreply.github.com>
2026-09-09 19:13:41 -0600

    Update pos_order.py

--- nilco_pos_multi_uom_price/models/pos_order.py
+++ nilco_pos_multi_uom_price/models/pos_order.py
@@ -6,19 +6,24 @@ from odoo import models, fields, api, _
 from odoo.tools import float_is_zero
 
 _logger = logging.getLogger(__name__)
-    
+
+
 class PosOrderLine(models.Model):
     _inherit = 'pos.order.line'
 
-    product_uom_id = fields.Many2one('uom.uom', string='Product UoM', related='')
+    product_uom_id = fields.Many2one('uom.uom', string='Product UoM')
 
     @api.depends('price_subtotal', 'total_cost')
     def _compute_margin(self):
         for line in self:
             line.margin = line.price_subtotal - line.total_cost
-            if line.product_uom_id.ratio != 0:
+            if line.product_uom_id and line.product_uom_id.ratio:
                 line.margin = line.margin / line.product_uom_id.ratio
-            line.margin_percent = not float_is_zero(line.price_subtotal, precision_rounding=line.currency_id.rounding) and line.margin / line.price_subtotal or 0
+            line.margin_percent = (
+                not float_is_zero(line.price_subtotal, precision_rounding=line.currency_id.rounding)
+                and line.margin / line.price_subtotal
+                or 0
+            )
 
     def _export_for_ui(self, orderline):
         res = super()._export_for_ui(orderline)
