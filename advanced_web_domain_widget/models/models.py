e3d19d591d9e79b3ee6866671b94aab6d1aa13f4
Colorize <97766820+colorizegt@users.noreply.github.com>
2026-09-10 10:35:32 -0600

    Update models.py

--- advanced_web_domain_widget/models/models.py
+++ advanced_web_domain_widget/models/models.py
@@ -7,13 +7,16 @@ class BaseModel(models.AbstractModel):
 
     @api.model
     def domain_name_search(self, name='', args=None, operator='ilike', limit=100):
-        res = self.sudo().name_search(name, args, operator, limit)
-        return res
+        """Odoo 19: name_search sigue existiendo con la misma firma básica."""
+        return self.sudo().name_search(name, args, operator, limit)
 
     @api.model
     def get_widget_count(self, args):
+        """Cuenta registros aplicando preparación de dominios."""
         domain_list = []
         for domain in args:
-            if isinstance(domain, tuple) or isinstance(domain, list):
-                domain_list += prepare_domain_v2(domain)
+            if isinstance(domain, (tuple, list)):
+                prepared = prepare_domain_v2(domain)
+                if prepared:
+                    domain_list += prepared
         return self.sudo().search_count(domain_list)
