5545919d61840b1dcf190783400e8d0a826b0af5
Colorize <97766820+colorizegt@users.noreply.github.com>
2026-09-09 19:58:24 -0600

    Update pos_session.py

--- pos_multicert_felgt/models/pos_session.py
+++ pos_multicert_felgt/models/pos_session.py
@@ -7,57 +7,23 @@ from odoo.exceptions import UserError
 class PosSession(models.Model):
     _inherit = 'pos.session'
 
-    def _loader_params_res_partner(self):
-        result = super()._loader_params_res_partner()
-        result['search_params']['fields'].append('fel_gt_passport_number')
-        result['search_params']['fields'].append('fel_gt_dpi_number')
+    def _load_pos_data_models(self, config):
+        """Odoo 19: Define qué modelos cargar en el POS.
+        
+        En Odoo 17 había que sobreescribir _pos_ui_models_to_load y añadir
+        _loader_params_* para cada modelo. En Odoo 19, la carga se delega
+        a cada modelo que implementa pos.load.mixin.
+        
+        Solo declaramos modelos custom (no estándar) que no tienen
+        pos.load.mixin, como fel_gt.tools.phrases.
+        """
+        result = super()._load_pos_data_models(config)
+        # Solo añadimos modelos custom, los estándar ya vienen cargados
+        # y sus campos extra se definen en el propio modelo con _load_pos_data_fields
+        for model in ['fel_gt.tools.phrases']:
+            if model not in result:
+                result.append(model)
         return result
-
-    def _pos_ui_models_to_load(self):
-        result = super()._pos_ui_models_to_load()
-        new_model = 'fel_gt.tools.phrases'
-        if new_model not in result:
-            result.append(new_model)
-        return result
-
-    def _loader_params_fel_gt_tools_phrases(self):
-        domain = [('id', 'in', self.config_id.fel_gt_phrases.ids)]
-        return {'search_params': {'domain': domain, 'fields': ['name'],}}
-
-    def _get_pos_ui_fel_gt_tools_phrases(self, params):
-        return self.env['fel_gt.tools.phrases'].search_read(**params['search_params'])
-    
-    def _loader_params_account_tax(self):
-        res = super()._loader_params_account_tax()
-        res["search_params"]["fields"].append("python_compute")
-        return res
-    
-    def _loader_params_res_users(self):
-        result = super()._loader_params_res_users()
-        result["search_params"]["fields"].append("fel_gt_cancel_in_pos")
-        result["search_params"]["fields"].append("fel_gt_motive_cancel_in_pos")
-        return result
-    
-    def _loader_params_product_product(self):
-        result = super()._loader_params_product_product()
-        result['search_params']['fields'].extend(["type","qty_available","virtual_available","stock_quant_ids"])
-        return result
-    
-    def _get_pos_ui_product_product(self, params):
-        products = super()._get_pos_ui_product_product(params)
-        for product in products:
-            stock_quant_ids = product.get('stock_quant_ids')
-            all_location = []
-            locations  = self.env['stock.location'].search([('id', '=', self.config_id.picking_type_id.default_location_src_id.id)])
-            all_location = list(locations.child_internal_location_ids.ids)
-            quants = self.env['stock.quant'].search([('id', 'in', stock_quant_ids), ('location_id.usage', '=', 'internal'), ('location_id', 'in', all_location)])
-
-            product['available_stock'] = 0.0
-            product['on_hand_stock'] = 0.0
-            for quant in quants:
-                product['available_stock'] += quant.available_quantity
-                product['on_hand_stock'] += quant.quantity
-        return products
 
     def _check_invoices_are_posted(self):
         if not self.config_id.fel_gt_active:
