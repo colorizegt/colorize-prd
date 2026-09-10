3d998209566343023e5062a4accbc5ad9d1a238a
Colorize <97766820+colorizegt@users.noreply.github.com>
2026-09-09 19:25:56 -0600

    Update pos_session.py

--- nilco_pos_multi_uom_price/models/pos_session.py
+++ nilco_pos_multi_uom_price/models/pos_session.py
@@ -4,47 +4,9 @@ from odoo import models, fields, api, _
 class PosSession(models.Model):
     _inherit = 'pos.session'
 
-    def _pos_ui_models_to_load(self):
-        result = super()._pos_ui_models_to_load()
-        new_model = 'product.multi.uom.price'
-        if new_model not in result:
-            result.append(new_model)
+    def _load_pos_data_models(self, config):
+        """Odoo 19: Define qué modelos cargar en el POS."""
+        result = super()._load_pos_data_models(config)
+        if 'product.multi.uom.price' not in result:
+            result.append('product.multi.uom.price')
         return result
-
-    def _loader_params_product_multi_uom_price(self):
-        return {
-            'search_params': {
-                'domain': [],
-                'fields': ['product_id', 'uom_id', 'price'],
-            },
-        }
-
-    def _get_pos_ui_product_multi_uom_price(self, params):
-        products_uom_price = self.env['product.multi.uom.price'].search_read(**params['search_params'])
-        product_uom_price = {}
-        for unit in products_uom_price:
-            product_id = unit['product_id'][0]
-            if product_id not in product_uom_price:
-                product_uom_price[product_id] = {'uom_id': {}}
-            product_uom_price[product_id]['uom_id'][unit['uom_id'][0]] = {
-                'id': unit['uom_id'][0],
-                'name': unit['uom_id'][1],
-                'price': unit['price'],
-            }
-        return product_uom_price
-
-    def _loader_params_product_product(self):
-        return {
-            'search_params': {
-                'domain': self.config_id._get_available_product_domain(),
-                'fields': [
-                    'display_name', 'lst_price', 'standard_price', 'categ_id',
-                    'pos_categ_ids', 'taxes_id', 'barcode', 'default_code',
-                    'to_weight', 'uom_id', 'description_sale', 'description',
-                    'product_tmpl_id', 'tracking', 'write_date', 'available_in_pos',
-                    'attribute_line_ids', 'active', 'image_128', 'combo_ids',
-                ],
-                'order': 'sequence,default_code,name',
-            },
-            'context': {'display_default_code': False},
-        }
