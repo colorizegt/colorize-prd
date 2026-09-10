506dce5d8f69be7051b24b46abdb37eba3fc471f
Colorize <97766820+colorizegt@users.noreply.github.com>
2026-09-10 11:14:26 -0600

    Update export.py

--- simplify_access_management/controllers/export.py
+++ simplify_access_management/controllers/export.py
@@ -1,22 +1,36 @@
 from odoo import http
 from odoo.exceptions import UserError
-from odoo.addons.web.controllers.export import Export
+from odoo.addons.web.controllers.export import Export as WebExport
 from odoo.http import request
 
-class Export(Export):
 
-    def fields_get(self, model):
-        fields=super().fields_get(model)
-        invisible_field_ids = request.env['hide.field'].search(
-                        [('access_management_id.company_ids', 'in', request.env.company.id),
-                         ('model_id.model', '=', model), ('access_management_id.active', '=', True),
-                        ('access_management_id.user_ids', 'in', request.env.user.id),
-                         ('invisible','=',True)])
+class Export(WebExport):
+
+    def fields_get(self, model, *args, **kwargs):
+        """Odoo 19: firma con *args/**kwargs para compatibilidad."""
+        fields = super().fields_get(model, *args, **kwargs)
+
+        invisible_field_ids = request.env['hide.field'].search([
+            ('access_management_id.company_ids', 'in', request.env.company.id),
+            ('model_id.model', '=', model),
+            ('access_management_id.active', '=', True),
+            ('access_management_id.user_ids', 'in', request.env.user.id),
+            ('invisible', '=', True)
+        ])
+
         if not invisible_field_ids:
             return fields
-        else :
-            for key, value in list(fields.items()):
-                for invisible_field in invisible_field_ids.field_id:
-                    if key == invisible_field.name and key != "id":
-                        del fields[key]
-            return fields
+
+        # Recopilar los nombres de campos a ocultar
+        invisible_field_names = set()
+        for invisible_field in invisible_field_ids:
+            for field in invisible_field.field_id:
+                if field.name:
+                    invisible_field_names.add(field.name)
+
+        # Eliminar los campos invisibles (excepto 'id')
+        for field_name in list(fields.keys()):
+            if field_name in invisible_field_names and field_name != 'id':
+                del fields[field_name]
+
+        return fields
