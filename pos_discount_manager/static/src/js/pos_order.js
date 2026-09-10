0aca104fb4707c3da69a5d0e7449d68b46a511b3
Colorize <97766820+colorizegt@users.noreply.github.com>
2026-09-09 19:45:23 -0600

    Update pos_order.js

--- pos_discount_manager/static/src/js/pos_order.js
+++ pos_discount_manager/static/src/js/pos_order.js
@@ -1,34 +1,37 @@
 /** @odoo-module */
-
 import { Order } from "@point_of_sale/app/store/models";
 import { patch } from "@web/core/utils/patch";
 
 patch(Order.prototype, {
-
-    setup() {
-        super.setup(...arguments);
-
-        this.discount_authorized = false;
-        this.discount_manager_id = false;
-        this.discount_manager_name = false;
+    /**
+     * Odoo 19: Exportamos los datos de autorización de descuento
+     * cuando la orden se envía al backend.
+     */
+    export_as_JSON() {
+        const json = super.export_as_JSON(...arguments);
+        
+        if (this.discount_authorized) {
+            json.discount_authorized = this.discount_authorized;
+            
+            if (this.discount_manager_id) {
+                json.discount_manager_id = this.discount_manager_id;
+            }
+            
+            if (this.discount_authorized_at) {
+                json.discount_authorized_at = this.discount_authorized_at;
+            }
+        }
+        
+        return json;
     },
 
     init_from_JSON(json) {
         super.init_from_JSON(...arguments);
-
-        this.discount_authorized = json.discount_authorized || false;
-        this.discount_manager_id = json.discount_manager_id || false;
-        this.discount_manager_name = json.discount_manager_name || false;
+        
+        if (json.discount_authorized) {
+            this.discount_authorized = json.discount_authorized;
+            this.discount_manager_id = json.discount_manager_id || false;
+            this.discount_authorized_at = json.discount_authorized_at || false;
+        }
     },
-
-    export_as_JSON() {
-        const json = super.export_as_JSON(...arguments);
-
-        json.discount_authorized = this.discount_authorized || false;
-        json.discount_manager_id = this.discount_manager_id || false;
-        json.discount_manager_name = this.discount_manager_name || false;
-
-        return json;
-    },
-
 });
