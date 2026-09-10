58f53e88f60302de8283f841eefa99d4f90884fa
Colorize <97766820+colorizegt@users.noreply.github.com>
2026-09-09 19:17:20 -0600

    Update product_multi_uom_price.py

--- nilco_pos_multi_uom_price/models/product_multi_uom_price.py
+++ nilco_pos_multi_uom_price/models/product_multi_uom_price.py
@@ -1,15 +1,18 @@
 from odoo import models, fields, api, _
 
 
-class multi_uom(models.Model):
+class ProductMultiUomPrice(models.Model):
     _name = 'product.multi.uom.price'
+    _description = 'Product Multi UoM Price'
 
-    product_id = fields.Many2one('product.template',string= 'Product',required=True,readonly=True)
+    product_id = fields.Many2one('product.template', string='Product', required=True, readonly=True)
     category_id = fields.Many2one(related='product_id.uom_id.category_id')
-    uom_id = fields.Many2one('uom.uom', string="Unit of Measure", domain="[('category_id', '=', category_id)]",required=True)
-    price = fields.Float(string='Price',required=True,digits='Product Price')   
+    uom_id = fields.Many2one('uom.uom', string="Unit of Measure",
+                             domain="[('category_id', '=', category_id)]", required=True)
+    price = fields.Float(string='Price', required=True, digits='Product Price')
+
     _sql_constraints = [
         ('product_multi_uom_price_uniq',
-         'UNIQUE (product_id,uom_id)',
-         _('UOM Product Must Be Unique !'))]
-    
+         'UNIQUE (product_id, uom_id)',
+         _('UOM Product Must Be Unique !'))
+    ]
