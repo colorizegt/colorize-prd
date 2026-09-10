86a1867c6a27b429ad67e7d51c2094bdd65591bd
Colorize <97766820+colorizegt@users.noreply.github.com>
2026-09-10 12:08:31 -0600

    Update search_bar_menu.js

--- simplify_access_management/static/src/js/search_bar_menu.js
+++ simplify_access_management/static/src/js/search_bar_menu.js
@@ -2,18 +2,31 @@
 import { SearchBarMenu } from "@web/search/search_bar_menu/search_bar_menu";
 import { patch } from "@web/core/utils/patch";
 import { useService } from "@web/core/utils/hooks";
-import { onWillStart, useState } from "@odoo/owl";
+import { onWillStart } from "@odoo/owl";
 
 patch(SearchBarMenu.prototype, {
   setup() {
     super.setup(...arguments);
     this.orm = useService("orm");
     onWillStart(async () => {
-      const res = await this.orm.call("access.management", "get_hidden_field", [
-        "",
-        this?.env?.searchModel?.resModel,
-      ]);
-      this.fields = this.fields.filter((ele) => !res.includes(ele.name));
+      const resModel = this.env?.searchModel?.resModel;
+      if (!resModel) {
+        return;
+      }
+      try {
+        const res = await this.orm.call("access.management", "get_hidden_field", [
+          "",
+          resModel,
+        ]);
+        if (this.fields && Array.isArray(this.fields)) {
+          this.fields = this.fields.filter((ele) => !res.includes(ele.name));
+        }
+      } catch (error) {
+        console.warn(
+          "[simplify_access_management] Error al filtrar campos:",
+          error
+        );
+      }
     });
   },
 });
