22bfdf7101bf12f757fc95d3a9ad7b5f70aa87c4
Colorize <97766820+colorizegt@users.noreply.github.com>
2026-09-10 11:06:42 -0600

    Update ir_model.py

--- simplify_access_management/models/ir_model.py
+++ simplify_access_management/models/ir_model.py
@@ -3,68 +3,55 @@
 from odoo import api, fields, models, tools, _
 
 
-class ir_model(models.Model):
+class IrModel(models.Model):
     _inherit = 'ir.model'
 
     abstract = fields.Boolean('Abstract', readonly=True)
 
-    # def name_get(self):
-    #     res = super().name_get()
-    #     if self._context.get('is_access_rights'):
-    #         res = []
-    #         for model in self:
-    #             res.append((model.id, "{} ({})".format(model.name, model.model)))
-    #     return res
-    @api.depends('name')
+    @api.depends('name', 'model')
     @api.depends_context('is_access_rights')
     def _compute_display_name(self):
         if not self.env.context.get('is_access_rights'):
             return super()._compute_display_name()
         for model in self:
-            new_name="{} ({})".format(model.name, model.model)
-            model.display_name = new_name
-      
-    
+            model.display_name = "{} ({})".format(model.name, model.model)
 
 
 class IrModelField(models.Model):
     _inherit = 'ir.model.fields'
 
-    @api.depends('model_id')
+    @api.depends('field_description', 'name', 'model_id.model')
     @api.depends_context('is_access_rights')
     def _compute_display_name(self):
         if not self.env.context.get('is_access_rights'):
             return super()._compute_display_name()
         for field in self:
-            new_name="{} => {} ({})".format(field.field_description, field.name, field.model_id.model)
-            field.display_name = new_name
+            field.display_name = "{} => {} ({})".format(
+                field.field_description, field.name, field.model_id.model
+            )
 
-class ir_ui_view(models.Model):
+
+class IrUiView(models.Model):
     _inherit = 'ir.ui.view'
 
-    @api.depends('model_id')
+    @api.depends('name', 'model')
     @api.depends_context('is_access_rights')
     def _compute_display_name(self):
         if not self.env.context.get('is_access_rights'):
             return super()._compute_display_name()
         for view in self:
-            new_name="{} ({})".format(view.name, view.model)
-            view.display_name = new_name
+            view.display_name = "{} ({})".format(view.name, view.model)
 
-    # def name_get(self):
-    #     res = super().name_get()
-    #     if self._context.get('is_access_rights'):
-    #         res = []
-    #         for view in self:
-    #             res.append((view.id, "{} ({})".format(view.name, view.model)))
-    #     return res
 
-class ir_module_module(models.Model):
+class IrModuleModule(models.Model):
     _inherit = 'ir.module.module'
 
     def _button_immediate_function(self, function):
-        res = super(ir_module_module, self)._button_immediate_function(function)
+        res = super()._button_immediate_function(function)
         if function.__name__ in ['button_install', 'button_upgrade']:
             for record in self.env['ir.model'].search([]):
-                record.abstract = self.env[record.model]._abstract
+                try:
+                    record.abstract = self.env[record.model]._abstract
+                except Exception:
+                    record.abstract = False
         return res
