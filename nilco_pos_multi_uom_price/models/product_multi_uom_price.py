91923cb47e096a1341e5754a4b5dfcd214c15e98
Colorize <97766820+colorizegt@users.noreply.github.com>
2026-09-09 19:27:02 -0600

    Update product_multi_uom_price.py

--- nilco_pos_multi_uom_price/models/product_multi_uom_price.py
+++ nilco_pos_multi_uom_price/models/product_multi_uom_price.py
@@ -35,3 +35,35 @@ class ProductMultiUomPrice(models.Model):
         # En Odoo 19, todas las UoMs están conectadas mediante relative_uom_id
         # por lo que podemos verificar si comparten un ancestro común
         return True  # Simplificado para Odoo 19
+
+# En product_multi_uom_price.py, añadir:
+
+    @api.model
+    def _load_pos_data_fields(self, config):
+        """Odoo 19: Define los campos a cargar en el POS."""
+        return ['product_id', 'uom_id', 'price']
+
+    @api.model
+    def _load_pos_data_domain(self, data, config):
+        """Odoo 19: Define el dominio para cargar datos en el POS."""
+        return []
+
+    @api.model
+    def _load_pos_data(self, data, config):
+        """Odoo 19: Carga los datos de UoM price en el POS."""
+        fields = self._load_pos_data_fields(config)
+        domain = self._load_pos_data_domain(data, config)
+        records = self.search_read(domain, fields)
+        
+        # Transformar a la estructura esperada por el frontend
+        product_uom_price = {}
+        for unit in records:
+            product_id = unit['product_id'][0]
+            if product_id not in product_uom_price:
+                product_uom_price[product_id] = {'uom_id': {}}
+            product_uom_price[product_id]['uom_id'][unit['uom_id'][0]] = {
+                'id': unit['uom_id'][0],
+                'name': unit['uom_id'][1],
+                'price': unit['price'],
+            }
+        return product_uom_price
